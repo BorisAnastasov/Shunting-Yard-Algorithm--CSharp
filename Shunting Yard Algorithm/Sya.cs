7d6dfe394d39728116c313b8a5acd913e09b448e
BorisAnastasov <boris_ywu@abv.bg>
2023-08-10 22:13:00 +0300

    reading data

---
+++ Shunting Yard Algorithm/Sya.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+public class ShuntingYardAlgorithm
+{
+	public ShuntingYardAlgorithm()
+	{
+		string input = Console.ReadLine();
+
+		char[] data = input.Split().ToCharArray();
+
+	}
+}
