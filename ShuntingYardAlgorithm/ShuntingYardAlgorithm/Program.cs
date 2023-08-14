baf616e75c0e78957c0ae5971ec2843020a2a64f
BorisAnastasov <boris_ywu@abv.bg>
2023-08-14 18:28:09 +0300

    commit

--- ShuntingYardAlgorithm/ShuntingYardAlgorithm/Program.cs
+++ ShuntingYardAlgorithm/ShuntingYardAlgorithm/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace ShuntingYardAlgorithm
+﻿using System.Linq;
+
+namespace ShuntingYardAlgorithm
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //readind data
+            string input = Console.ReadLine();
+
+            var data = input.Split().ToList<char>();
+
+            var output = new Queue<char>();
+
+            var stack = new Stack<char>();
+
+            while (data.Length > 0)
+            {
+                if (char.IsDigit(data[0]))
+                {
+                    output.Enqueue();
+                }
+                else if ()
+                {
+
+                }
+            }
         }
     }
 }
