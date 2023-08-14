bb839e89b82a745aa75754a8db020347705f2782
BorisAnastasov <boris_ywu@abv.bg>
2023-08-14 18:23:45 +0300

    commit

---
+++ ShuntingYardAlgorithm/ShuntingYardAlgorithm/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ShuntingYardAlgorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
